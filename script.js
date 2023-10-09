292517989cdca34857d576f62d1017f916b635cf
Vendula Bezáková <vendula.bezakova@gmail.com>
2023-10-09 13:36:48 +0200

    ukoly

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const jmeno = "Vendula"
+/*const jmeno = "Vendula"
 
-document.body.innerHTML = "<h1>"  + jmeno + "</h1>"
+document.body.innerHTML = "<h1>"  + jmeno + "</h1>";
+document.body.innerHTML = "Náhodné číslo je " + Math.random();*/
+
+/*const wageInEur = 20;
+const rate = 24.55;
+const wageInCzk = Math.round(wageInEur * rate);
+
+document.body.innerHTML = "<h1> Mzda v korunách: " + wageInCzk + "Kč </h1>"*/
+
+/*const start = 15;
+const delka = 7;
+const konec = (start + delka) % 24;
+document.body.innerHTML = "Závod skončí v " + konec + " hodin."*/
+
+const listek = 12
+const navstevniku = 174
+const predstaveni = 15
+
+const prijem = listek*navstevniku*predstaveni
+
+document.body.innerHTML = prijem
